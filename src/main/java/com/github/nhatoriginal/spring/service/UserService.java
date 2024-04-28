1a48e86dc1f98f1ac73d95f19388b19e3f6c99b9
Nhat-Original <nhatoriginal@gmail.com>
2024-04-28 12:54:06 +0700

    fix: fix login and register api to return response instead of throwing errors

--- src/main/java/com/github/nhatoriginal/spring/service/UserService.java
+++ src/main/java/com/github/nhatoriginal/spring/service/UserService.java
@@ -4,37 +4,39 @@ import com.github.nhatoriginal.spring.model.User;
 import com.github.nhatoriginal.spring.repository.UserRepository;
 import com.github.nhatoriginal.spring.security.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
 
+  private final UserRepository userRepository;
 
-    private final UserRepository userRepository;
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+  @Override
+  public UserDetails loadUserByUsername(String email) {
+    User user = userRepository.findByEmail(email);
+    if (user == null) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "email does not exists");
     }
+    return new CustomUserDetails(user);
+  }
 
-    @Override
-    public UserDetails loadUserByUsername(String email) {
-        User user = userRepository.findByEmail(email);
-        if (user == null) {
-            throw new UsernameNotFoundException(email);
-        }
-        return new CustomUserDetails(user);
-    }
-    public User findByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
-    public User getUserById(String id) {
-        return userRepository.findById(UUID.fromString(id)).orElse(null);
-    }
+  public User findByEmail(String email) {
+    return userRepository.findByEmail(email);
+  }
 
+  public User getUserById(String id) {
+    return userRepository.findById(UUID.fromString(id)).orElse(null);
+  }
 
 }
